f51b31a18516ceffb217a806fec0caa4e199a18f
rabtman <zhangjm05@gmail.com>
2018-09-29 18:44:37 +0800

    新增普通用法

--- rx2-example/src/main/java/com/rabtman/MainActivity.java
+++ rx2-example/src/main/java/com/rabtman/MainActivity.java
@@ -2,8 +2,14 @@ package com.rabtman;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import com.rabtman.backpressure.BackpressureExample;
 import com.rabtman.example.R;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +19,37 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    //normal use
+    Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+      @Override
+      public void subscribe(ObservableEmitter<String> e) throws Exception {
+        e.onNext("onNext");
+        e.onComplete();
+      }
+    });
+    Observer observer = new Observer<String>() {
+      @Override
+      public void onSubscribe(Disposable d) {
+        Log.d(TAG, "onSubscribe:" + d.isDisposed());
+      }
+
+      @Override
+      public void onNext(String o) {
+        Log.d(TAG, "onNext:" + o);
+      }
+
+      @Override
+      public void onError(Throwable e) {
+        Log.d(TAG, "onError:" + e.getMessage());
+      }
+
+      @Override
+      public void onComplete() {
+        Log.d(TAG, "onComplete:");
+      }
+    };
+    observable.subscribe(observer);
 
     //关于背压
     BackpressureExample backpressureExample = new BackpressureExample();
